3e36b528d0cd8f24298acaa068bc7681e6908deb
jrmygp <fat.snorlax17@gmail.com>
2022-03-23 14:45:12 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+const dotenv = require("dotenv")
+const cors = require("cors")
+
+dotenv.config();
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+const { employeeRoutes, authRoutes } = require("./routes");
+
+app.use("/employees", employeeRoutes)
+app.use("/auth", authRoutes)
+
+app.listen(PORT, () => {
+  console.log("Listening in port", PORT)
+})
